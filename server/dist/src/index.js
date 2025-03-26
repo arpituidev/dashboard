273ccfc7248c4fc5320e6d510de61fd12085e09e
arpituidev <arpituidev@gmail.com>
2025-03-26 19:10:46 -0400

    dashboard data

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,20 +10,19 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const body_parser_1 = __importDefault(require("body-parser"));
 /* ROUTE IMPORTS */
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, helmet_1.default)());
-app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use((0, morgan_1.default)('common'));
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 /* ROUTES */
-app.get('/hello', (req, res) => {
-    res.send('hello world');
-});
+app.use("/dashboard", dashboardRoutes_1.default); // https://localhost:8000/dashboard
 /* SERVER */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,28 @@
 import express from "express";
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import cors from 'cors';
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 /* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
-app.use(morgan('common'));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get('/hello', (req, res) => {
-    res.send('hello world')
-});
+app.use("/dashboard", dashboardRoutes); // https://localhost:8000/dashboard
 
 /* SERVER */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
